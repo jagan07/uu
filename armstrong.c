8db2ac4df8e665c7b20be9d4d7bcf503eaf9d084
jagan07 <47105628+jagan07@users.noreply.github.com>
2019-03-21 16:22:27 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+printf("enter the number to find armstromg");
+scanf("%d",&onum);
+num=onum;
+while(num<=100000)
+{
+while(num!=0)
+{
+rem=num%10;
+result+=rem*rem*rem;
+num=num/10;
+}
+}
+if(num==onum)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+getch();
+}
